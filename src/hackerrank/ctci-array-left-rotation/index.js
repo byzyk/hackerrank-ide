6e6aca26d3d7c0d093b59b05cf7817bee826b17f
byzyk <bohdan.kh@gmail.com>
2018-01-17 11:36:45 +0400

    rotation solved

--- src/hackerrank/ctci-array-left-rotation/index.js
+++ src/hackerrank/ctci-array-left-rotation/index.js
@@ -3,7 +3,16 @@ process.stdin.on('end', main);
 /////////////// ignore above this line ////////////////////
 
 function main() {
-  let result = null;
+  const n_temp = readLine().split(' ');
+  const n = parseInt(n_temp[0]);
+  const k = parseInt(n_temp[1]);
+  let a = readLine().split(' ');
+  a = a.map(Number);
 
-  process.stdout.write('' + result + '\n');
+  const newArr = [];
+  for (let i = 0; i < n; i++) {
+    newArr[(i + n - k) % n] = a[i];
+  }
+
+  process.stdout.write('' + newArr.join(' ') + '\n');
 }
